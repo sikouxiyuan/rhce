c008cad94883436df299ffb98168c6c395df9a9a
sikouxiyuan <huizhazha123@gmail.com>
2022-03-27 19:52:03 +0800

    1
    all playbook for RHCR 2021.12

---
+++ adhoc.sh
@@ -0,0 +1,2 @@
+ansible all -m yum_repository -a 'name="EX294_BASE" description="EX294 base software" baseurl="http://content/rhel8.0/x86_64/dvd/BaseOS" gpgcheck=yes gpgkey="http://content/rhel8.0/x86_64/dvd/RPM-GPG-KEY-redhat-release" enabled=yes'
+ansible all -m yum_repository -a 'name="EX294_STREAM" description="EX294 stream software" baseurl="http://content/rhel8.0/x86_64/dvd/AppStream" gpgcheck=yes gpgkey="http://content/rhel8.0/x86_64/dvd/RPM-GPG-KEY-redhat-release" enabled=yes'
